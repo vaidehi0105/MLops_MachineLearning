fe60786395862e081762ead09ec609317faf772d
vaidehi0105 <vaidehibedekar0105@gmail.com>
2023-10-06 00:29:29 +0530

    web app added in home.html under templates

--- Step_2_DockerContainerization/app.py
+++ Step_2_DockerContainerization/app.py
@@ -25,13 +25,14 @@ def predict_api():
     print(result)
     return jsonify(result)
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     data=[float(x) for x in request.form.values()]
-#     final_input=scalar.transform(np.array(data).reshape(1,-1))
-#     print(final_input)
-#     output=model.predict(final_input)[0]
-#     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)
+    result=output[0].tolist()
+    return render_template("home.html",prediction_text="The Diabetes prediction is {}".format(result))
 
 if __name__=="__main__":
     app.run(debug=True)
